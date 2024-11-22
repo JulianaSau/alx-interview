fdd73003fd6c5f06df2dbe8664bf5a78aa784564
Juliana Sau <julienyange@gmail.com>
2024-11-22 11:27:54 +0300

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,30 +1,25 @@
 #!/usr/bin/node
-/*
-Write a script that prints all characters of a Star Wars movie
-*/
-
 const request = require('request');
+const filmURL = 'https://swapi-api.hbtn.io/api';
 
-const req = (arr, i) => {
-  if (i === arr.length) return;
-  request(arr[i], (err, response, body) => {
+if (process.argv.length > 2) {
+  request(`${filmURL}/films/${process.argv[2]}/`, (err, _, body) => {
     if (err) {
-      throw err;
-    } else {
-      console.log(JSON.parse(body).name);
-      req(arr, i + 1);
+      console.log(err);
     }
+    const charactersURL = JSON.parse(body).characters;
+    const characterName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promisErr, __, charactersReqBody) => {
+          if (promisErr) {
+            reject(promisErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
+
+    Promise.all(characterName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
   });
-};
-
-request(
-  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
-  (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      const chars = JSON.parse(body).characters;
-      req(chars, 0);
-    }
-  }
-);
+}
